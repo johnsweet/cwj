d508482514460e5117558b68d3f76af3f2dbdb73
johnsweet <johnsweet@163.com>
2015-01-26 11:05:53 +0800

    first create

---
+++ 辅助填报系统/js/action.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+<script language="javascript">
+<!--
+	function comfirm_location(url){
+		question = confirm("您的卫生许可证真的过期了吗？");
+		if (question){
+			window.location.href = url;
+		}
+	}
+//-->
+</script>
